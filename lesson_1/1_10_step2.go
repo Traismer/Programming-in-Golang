69c9ce14604acf158c2b0d1735de7098c5a3f9ca
Traismer <alekstrais@yandex.ru>
2024-07-12 15:42:43 +0300

    update import

--- lesson_1/1_10_step2.go
+++ lesson_1/1_10_step2.go
@@ -1,17 +1,17 @@
 package main
 
-import . "fmt"
+import "fmt"
 
 /*
 Напишите программу, которая выводит квадраты натуральных чисел от 1 до 10. Квадрат каждого числа должен выводится в новой строке.
 */
 
-func SquaresOfNaturalNumbers() {
-	for number := 1; number <= 10; number++ {
-		Println(number * number)
+func SquaresOfNaturalNumbers(number int) {
+	for i := 1; i <= number; i++ {
+		fmt.Println(i * i)
 	}
 }
 
 func main() {
-	SquaresOfNaturalNumbers()
+	SquaresOfNaturalNumbers(10)
 }
